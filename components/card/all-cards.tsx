1d16c54695e2745161ca84ff2450c9a0816df1a6
DevKraken <somanbandesha.sb@gmail.com>
2023-10-16 16:35:31 -0700

    S

---
+++ components/card/all-cards.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+export function AllCards() {
+    return (
+        <div className="w-100 h-80 border border-zinc-400 border-dashed p-4 rounded flex flex-col justify-center items-center">
+            asdf
+        </div>
+    )
+}
